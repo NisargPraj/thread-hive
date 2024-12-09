f9e5ef0d8b27e6a4e0aa17f1602dd6fa8d6fba48
AntVann <anthonyjustinvann@gmail.com>
2024-12-09 13:33:10 -0800

    Fixed health

--- backend/post-service/post/urls.py
+++ backend/post-service/post/urls.py
@@ -8,18 +8,22 @@ from .views import (
     HashtagViewSet,
     HashtagGeneratorViewSet,
     DummyViewSet,
+    HealthCheckView,
 )
 
 router = DefaultRouter()
 
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'following', SpecificPostViewSet, basename='following-posts')
-router.register(r'likes', LikeViewSet, basename='like')
-router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'hashtags', HashtagViewSet, basename='hashtag')
-router.register(r'hashtags/generate', HashtagGeneratorViewSet, basename='generate-hashtags')
-router.register(r'dummy', DummyViewSet, basename='dummy')
+router.register(r"posts", PostViewSet, basename="post")
+router.register(r"following", SpecificPostViewSet, basename="following-posts")
+router.register(r"likes", LikeViewSet, basename="like")
+router.register(r"comments", CommentViewSet, basename="comment")
+router.register(r"hashtags", HashtagViewSet, basename="hashtag")
+router.register(
+    r"hashtags/generate", HashtagGeneratorViewSet, basename="generate-hashtags"
+)
+router.register(r"dummy", DummyViewSet, basename="dummy")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("health/", HealthCheckView.as_view(), name="health_check"),
 ]
